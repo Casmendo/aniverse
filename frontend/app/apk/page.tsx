2e0116db4f4d30789460d7cb0c8651dae2b602b3
Casmendo <isahmusa9921@gmail.com>
2026-05-23 12:54:32 +0100

    chore: update APK download link to 4x0taj

--- frontend/app/apk/page.tsx
+++ frontend/app/apk/page.tsx
@@ -27,7 +27,7 @@ export default function APKPage() {
         clearInterval(iv); 
         setDone(true); 
         // Trigger real download
-        window.location.assign('https://files.catbox.moe/orvwu7.apk');
+        window.location.assign('https://files.catbox.moe/4x0taj.apk');
       }
       setProgress(Math.min(100,p));
     }, 140);
