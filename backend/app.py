ef134b19e259d52f5ac27b86ae653145e3acb8d2
Casmendo <isahmusa9921@gmail.com>
2026-05-28 22:39:58 +0100

    Fix CORS blocking custom API key header

--- backend/app.py
+++ backend/app.py
@@ -51,7 +51,7 @@ CORS(app,
               'http://localhost:3001','http://127.0.0.1:3001',
               'http://localhost', 'capacitor://localhost'],
      supports_credentials=True,
-     allow_headers=['Content-Type','Authorization'],
+     allow_headers=['Content-Type','Authorization','X-Aniverse-Key'],
      methods=['GET','POST','DELETE','OPTIONS'])
 
 db      = SQLAlchemy(app)
